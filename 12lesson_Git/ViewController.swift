fdc1c5ce7ae0d2a0903a83665ce611a91adc353c
Mator <mator1@tut.by>
2022-08-08 22:03:54 +0300

    secondCommit

---
+++ 12lesson_Git/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  12lesson_Git
+//
+//  Created by Macbook M1 on 8.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let int = 1 
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
